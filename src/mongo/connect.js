ac92aba67f4cf9bfb2ddea68905351ace97077ed
Craig Miller <craigmiller160@gmail.com>
2020-06-14 15:42:21 -0400

    Updated connect.js to not depend on cloud config server

--- src/mongo/connect.js
+++ src/mongo/connect.js
@@ -1,17 +1,54 @@
 const { MongoClient } = require('mongodb');
-const { cloudConfig, MONGO_HOST, MONGO_AUTH_DB, MONGO_PASS, MONGO_USER, MONGO_PORT } = require('../config');
+
 const logger = require('../utils/logger');
 
+const getConfig = async () => {
+    if (process.env.USE_CONFIG_SERVER) {
+        const { cloudConfig, MONGO_HOST, MONGO_AUTH_DB, MONGO_PASS, MONGO_USER, MONGO_PORT } = require('../config');
+        await cloudConfig.init();
+        const mongoUser = cloudConfig.config[MONGO_USER];
+        const mongoPass = cloudConfig.config[MONGO_PASS];
+        const mongoAuthDb = cloudConfig.config[MONGO_AUTH_DB]
+        const mongoHost = cloudConfig.config[MONGO_HOST];
+        const mongoPort = cloudConfig.config[MONGO_PORT];
+
+        return {
+            mongoUser,
+            mongoPass,
+            mongoAuthDb,
+            mongoHost,
+            mongoPort
+        };
+    } else {
+        const mongoUser = process.env.MONGO_USER;
+        const mongoPass = process.env.MONGO_PASS;
+        const mongoAuthDb = process.env.MONGO_AUTH_DB;
+        const mongoHost = process.env.MONGO_HOST;
+        const mongoPort = process.env.MONGO_PORT;
+
+        return {
+            mongoUser,
+            mongoPass,
+            mongoAuthDb,
+            mongoHost,
+            mongoPort
+        };
+    }
+};
+
 const connect = async (handler) => {
-    await cloudConfig.init();
-    const mongoUser = cloudConfig.config[MONGO_USER];
-    const mongoPass = cloudConfig.config[MONGO_PASS];
+    const {
+        mongoUser,
+        mongoPass,
+        mongoAuthDb,
+        mongoHost,
+        mongoPort
+    } = await getConfig();
+
     const dbName = process.env.MONGO_DATABASE;
     const profile = process.env.ACTIVE_PROFILE;
     const mongoDb = `${dbName}_${profile}`;
-    const mongoAuthDb = cloudConfig.config[MONGO_AUTH_DB]
-    const mongoHost = cloudConfig.config[MONGO_HOST];
-    const mongoPort = cloudConfig.config[MONGO_PORT];
+
     const mongoConnectionString = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:${mongoPort}/${mongoDb}?authSource=${mongoAuthDb}`;
     const options = {
         useUnifiedTopology: true
